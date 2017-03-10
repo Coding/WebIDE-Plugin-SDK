8335a846d1d6dc6d46977c8627e06ca1b8cf9a08
郑鑫祺 <candy.zhengxq@gmail.com>
2017-03-10 20:32:05 +0800

    修复编译脚本的强制软链

--- .script/getPackage.sh
+++ .script/getPackage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ ${PLUGIN} ]; then
     yarn add codingIdePlugin@${PLUGIN} --force
-    ln -s $(pwd)/node_modules/* node_modules/codingIdePlugin/node_modules
+    ln -fs $(pwd)/node_modules/* node_modules/codingIdePlugin/node_modules
 fi

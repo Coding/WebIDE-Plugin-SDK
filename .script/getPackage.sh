0d9780abcc444413681aba6ca722a6a8392f3a2c
candy <candy.zhengxq@gmail.com>
2017-03-10 16:17:34 +0800

    remove extra code

--- .script/getPackage.sh
+++ .script/getPackage.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -eo pipefail
 if [ ${PLUGIN} ]; then
     yarn add codingIdePlugin@${PLUGIN} --force
     ln -s $(pwd)/node_modules/* node_modules/codingIdePlugin/node_modules

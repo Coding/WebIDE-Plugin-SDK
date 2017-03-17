d2b42412c5de8225fe8455e1ce62481e7eb5e7df
郑鑫祺 <candy.zhengxq@gmail.com>
2017-03-17 15:18:01 +0800

    修改编译脚本使用自己的shell clone package

--- .script/getPackage.sh
+++ .script/getPackage.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 if [ ${PLUGIN} ]; then
-    yarn add codingIdePlugin@${PLUGIN} --force
-    ln -fs $(pwd)/node_modules/* node_modules/codingIdePlugin/node_modules
+    echo "pluginUrl:"${PLUGIN}
+    if [ -d "node_modules/codingIdePlugin" ]; then
+        rm -rf node_modules/codingIdePlugin
+    fi
+    git clone ${PLUGIN} node_modules/codingIdePlugin
+    cd node_modules/codingIdePlugin && yarn install
+    # ln -fs $(pwd)/node_modules/* node_modules/codingIdePlugin/node_modules
 fi

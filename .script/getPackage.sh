4c96d147eda6d68dc99d24c8a18d6b66cfc65cbf
郑鑫祺 <candy.zhengxq@gmail.com>
2017-07-19 17:19:18 +0800

    修改 fetch packages 暂时删除原有目录

--- .script/getPackage.sh
+++ .script/getPackage.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 if [ ${PLUGIN} ]; then
     echo "pluginUrl:"${PLUGIN}
-    if [ -d "node_modules/codingIdePlugin" ]; 
-        then
-        echo "===git status===="
-        git -C node_modules/codingIdePlugin status
-        echo "===git reset===="  
-        git reset --hard              
-        echo "===git pull===="
-        git -C node_modules/codingIdePlugin pull
-        else
-        echo "===git clone===="
-        git clone ${PLUGIN} node_modules/codingIdePlugin
+    echo "===remove origin===="    
+    if [ -d "node_modules/codingIdePlugin" ]; then
+        rm -rf node_modules/codingIdePlugin
     fi
+    echo "===git clone====" 
+    git clone ${PLUGIN} node_modules/codingIdePlugin
     echo "===install package dependency===="
     cd node_modules/codingIdePlugin && yarn install
     echo "===start build package===="    

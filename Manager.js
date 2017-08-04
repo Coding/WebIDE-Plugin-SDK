bb9474ebd6f5d1bf55620fee23d5ea8c2a08a06a
郑鑫祺 <candy.zhengxq@gmail.com>
2017-08-04 16:34:43 +0800

    增加Manager

---
+++ Manager.js
@@ -0,0 +1,16 @@
+import { mobx } from './modules';
+
+export default class {
+  pluginWillMount() {
+
+  }
+  pluginWillUnmount() {
+
+  }
+  autoRun() {
+
+  }
+  // 60fs doing something
+//   render() {
+//   }
+}

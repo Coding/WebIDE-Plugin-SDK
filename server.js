6f2f0e03151d88fe7fc8f5e9550db84e8331750c
Yang Zhen <yangzhen@coding.net>
2017-10-19 16:59:00 +0800

    Change localhost to ide.test

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: 'http://localhost:8060',
+  origin: ['http://localhost:8060','http://ide.test:8060'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
@@ -70,7 +70,7 @@ app.get('/packages/:pkgName/:version/manifest.json', (req, res) => {
 
 const webpackDevInstance = require('webpack-dev-middleware')(compiler, {
   publicPath: `/packages/${codingPackage.name}/${codingPackage.version}`,
-  headers: { 'Access-Control-Allow-Origin': 'http://localhost:8060' },
+  headers: { 'Access-Control-Allow-Origin': 'http://ide.test:8060' },
   historyApiFallback: true,
   quiet: false,
   noInfo: false,

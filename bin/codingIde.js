a6500e309e29b7a596ab948ffcb66a808f6948f0
sakuraash <841105127@qq.com>
2018-10-08 10:43:29 +0800

    update config

--- bin/codingIde.js
+++ bin/codingIde.js
@@ -4,7 +4,11 @@ const exec = require('child_process').exec;
 const path = require('path');
 const stepFactory = require('../lib/step');
 const parse = require('../lib/parse');
-const generateI18n = require('../.script/generateI18n')
+const log4js = require('log4js');
+// const generateI18n = require('../.script/generateI18n')
+
+const logger = log4js.getLogger('webide-sdk');
+logger.level = 'debug';
 
 function execPromise(command, options = {}, callback) {
   return new Promise((resolve, reject) => {
@@ -19,13 +23,13 @@ function execPromise(command, options = {}, callback) {
   });
 }
 
-async function i18n() {
-  process.env.NODE_ENV = 'development';
-  const PACKAGE_DIR = process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
-  process.env.I18nTargetRoot = process.env.I18nTargetRoot || path.resolve(__dirname, '../../../', 'i18n');
-  console.log('PACKAGE_DIR', PACKAGE_DIR)
-  generateI18n(`${PACKAGE_DIR}/src`)
-}
+// async function i18n() {
+//   process.env.NODE_ENV = 'development';
+//   const PACKAGE_DIR = process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
+//   process.env.I18nTargetRoot = process.env.I18nTargetRoot || path.resolve(__dirname, '../../../', 'i18n');
+//   console.log('PACKAGE_DIR', PACKAGE_DIR)
+//   generateI18n(`${PACKAGE_DIR}/src`)
+// }
 
 async function serve() {
   const step = stepFactory();
@@ -34,7 +38,7 @@ async function serve() {
   await step(`[${stepNum++}] get current version`, () => {
     return execPromise('git describe --always --tags')
     .then((stdout) => {
-      console.log(`current version is ${stdout}`);
+      logger.info(`Current Git version is ${stdout}`);
       process.env.VERSION = stdout.replace(/\s+/, '');
       process.env.PACKAGE_DIR = process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');    
       return true;
@@ -46,38 +50,47 @@ async function serve() {
 }
 
 
-async function start() {
+async function start(packageDir) {
   process.env.NODE_ENV = 'development';
-  process.env.PACKAGE_DIR = process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
+  process.env.PACKAGE_DIR = packageDir || process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
   require('../server.js')
 }
-async function build() {
+async function build(packageDir) {
+  console.log(`
+     /$$      /$$           /$$     /$$$$$$ /$$$$$$$  /$$$$$$$$
+  | $$  /$ | $$          | $$    |_  $$_/| $$__  $$| $$_____/
+  | $$ /$$$| $$  /$$$$$$ | $$$$$$$ | $$  | $$  \ $$| $$      
+  | $$/$$ $$ $$ /$$__  $$| $$__  $$| $$  | $$  | $$| $$$$$   
+  | $$$$_  $$$$| $$$$$$$$| $$  \ $$| $$  | $$  | $$| $$__/   
+  | $$$/ \  $$$| $$_____/| $$  | $$| $$  | $$  | $$| $$      
+  | $$/   \  $$|  $$$$$$$| $$$$$$$/$$$$$$| $$$$$$$/| $$$$$$$$
+  |__/     \__/ \_______/|_______/______/|_______/ |________/`);
   const step = stepFactory();
   let stepNum = 1;
-  await step(`[${stepNum++}] get current version`, () => {
+  await step(`[${stepNum++}] get Project current version`, () => {
     return execPromise('git describe --always --tags')
     .then((stdout) => {
-      console.log(`current version is ${stdout}`);
+      logger.info(`current Project version is ${stdout}`);
       process.env.VERSION = stdout.replace(/\s+/, '');
       return true;
     });
   });
   await step(`[${stepNum++}] clean up dist folder`, () => {
     return execPromise('node_modules/.bin/rimraf dist/**').then(() => {
-      console.log('cleared');
+      logger.info('cleared');
       return true;
     });
   });
   await step(`[${stepNum++}] build source`, () => {
-    process.env.PACKAGE_DIR = process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
-    console.log('current source dir', process.env.PACKAGE_DIR, process.env.VERSION);
+    process.env.PACKAGE_DIR = packageDir || process.env.PACKAGE_DIR || path.resolve(__dirname, '../../../');
+    logger.info('current source dir', process.env.PACKAGE_DIR, process.env.VERSION);
     return execPromise(`webpack --config ${path.resolve(__dirname, '../', 'webpack.production.config.js')} --progress --profile --colors`)
     .then((out) => {
-      console.log(out);
+      logger.info(out);
       return true;
     })
     .catch((err, stdout, stderr) => {
-      console.log(err);
+      logger.error(err);
       return false;
     });
   });
@@ -95,11 +108,15 @@ const cli = parse(process.argv);
 
 
 if (cli.args[0] === 'build') {
-  build();
+  if (!!cli.args[1] && cli.args[1] !== '') {
+    build(cli.args[1]);
+  }
 }
 
 if (cli.args[0] === 'start') {
-  start();
+  if (!!cli.args[1] && cli.args[1] !== '') {
+    start(cli.args[1]);
+  }
 }
 
 if (cli.args[0] === 'i18n') {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -65,6 +65,7 @@ const defaultConfig = {
       callback();
     },
     generalExtenalAlias,
+    'codingide',
   ],
 
 };
